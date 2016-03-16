969f426df7f446542b05a0dd06593068918aab9e
John Vella <johnv@mindarc.com.au>
2016-03-16 11:30:58 +1100

    Update to Magento Controller for Response Body and Header setting

--- app/code/community/MindArc/FatZebra/controllers/IndexController.php
+++ app/code/community/MindArc/FatZebra/controllers/IndexController.php
@@ -9,11 +9,25 @@
 class MindArc_FatZebra_IndexController extends Mage_Core_Controller_Front_Action {
 
     public function indexAction() {
-         $session = $this->getRequest()->getParam('io_bb');
-        if($session!=null && $session!="")
+        $response = array();
+        $session = $this->getRequest()->getParam('io_bb');
+        if($session!=null && $session!=""){
+            $response['code'] = 'success';
             Mage::getSingleton('core/session')->setFatzebraFraud($session);
-        $this->loadLayout();
-        $this->renderLayout();
+        }else{
+            $response['code'] = 'error';
+        }
+
+
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        if ($response['code'] != 'success') {
+            $this->getResponse()->setHeader('HTTP/1.0', '404', true);
+        } else {
+            $this->getResponse()->setHeader('HTTP/1.0', '200', true);
+        }
+        $jsonData = json_encode($response);
+        $this->getResponse()->setBody($jsonData);
+
     }
     public function sessionAction(){
         echo Mage::getSingleton('core/session')->getFatzebraFraud();
